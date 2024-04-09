a01de82bfb06c670824ce5a408c629643a32de10
SandeepKumarRudhravram <148599608+SandeepKumarRudhravram@users.noreply.github.com>
2024-04-09 14:47:56 -0500

    dark mode

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
+
 from portal import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=8081)
+    port = int(os.environ.get('PORT',8001))
+    app.run(host='0.0.0.0',port=port,debug=True)
